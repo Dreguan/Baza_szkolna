6a9907932da93a8542db99cf83162622e2f98a64
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-06-23 17:52:04 +0200

    Uporządkowanie kodu

--- bazaszkolna.py
+++ bazaszkolna.py
@@ -11,12 +11,11 @@ class Uczen:
         self.klasa =input()
         if self.klasa not in klasy:
             klasy[self.klasa] = []
-        klasy[self.klasa].append(self) #self to obiekt klasy uczeń
+        klasy[self.klasa].append(self)
     def wyswietl(self):
         for nauczyciel in nauczyciele_klas[self.klasa]:
             print(nauczyciel.przedmiot)
             print(nauczyciel.nazwa)
-        #print("Uczeń: {} - Klasa: {}.".format(self.nazwa, self.klasa))
 
 
 class Nauczyciel:
@@ -26,7 +25,7 @@ class Nauczyciel:
         self.klasy = []
         while True:
             klasa = input()
-            if klasa == "": #tak samo zadziała if not klasa
+            if klasa == "":
                 break
             self.klasy.append(klasa)
             if klasa not in nauczyciele_klas:
@@ -36,7 +35,6 @@ class Nauczyciel:
         for klasa in self.klasy:
             if klasa in wychowawcy_klas:
                 print(wychowawcy_klas[klasa].nazwa)
-        #print("Nauczyciel: {} - Przedmiot: {} -  Klasy: {}.".format(self.nazwa, self.przedmiot, self.klasy))
 
 
 class Wychowawca:
@@ -53,7 +51,6 @@ class Wychowawca:
         for klasa in self.klasy:
             for osoba in klasy[klasa]:
                 print(osoba.nazwa)
-        #print("Wychowawca: {} -  Klasy: {}.".format(self.nazwa, self.klasy))
 
 
 while True:
@@ -61,23 +58,14 @@ while True:
     if rodzaj == "koniec":
         break
     if rodzaj == "nauczyciel":
-        osoba = Nauczyciel() #tworzy nowy obiekt klasy Nauczyciel
+        osoba = Nauczyciel()
     if rodzaj == "wychowawca":
         osoba = Wychowawca()
     if rodzaj == "uczen":
         osoba = Uczen()
-    #osoba.wyswietl()
     if osoba.nazwa not in osoby:
         osoby[osoba.nazwa] = []
     osoby[osoba.nazwa].append(osoba)
-    #osoby[osoba.nazwa] = osoba #z obiektu osoba wyciągnęliśmy nazwę
-
-for nazwa, osoba in osoby.items(): #items dostaje się jednocześnie i do klucza i do wartości
-    print(nazwa)
-#    print(osoba.__dict__) #przekształca dany obiekt w słownik dzięki czemu widzimy co rzeczywiście siedzi wewątrz obiektu
-
-#print(osoby)
-print(klasy.items())
 
 if sys.argv[1] in osoby:
     for osoba in osoby[sys.argv[1]]:
